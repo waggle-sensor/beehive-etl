d10e8b98a5b0a3b77babf6a8a94dbad20f1c9ebc
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-04-09 11:59:24 -0500

    changing select all keys + filter with explicit generate expected keys

--- beehive-digest-tools/update-recent-digests.sh
+++ beehive-digest-tools/update-recent-digests.sh
@@ -6,11 +6,11 @@ function compile() {
   mkdir -p /storage/datasets/v1 /storage/datasets/v2 /storage/plugins
   rm -rf /storage/datasets/v1/* /storage/datasets/v2/* /storage/digests/*
 
-  python list-datasets-v1 | python filter-last-day | python export-recent-datasets-v1 --since $WINDOW -p 8 /storage/datasets/v1
+  python list-keys-v1 --end today --periods 3 | python list-datasets-v1 | python export-recent-datasets-v1 --since $WINDOW -p 8 /storage/datasets/v1
 
   # TODO figure out how to handle sdf / beehive plugins
   # TODO look into what the -p support is
-  python list-datasets-v2 | python filter-last-day | python export-recent-datasets-v2 --since $WINDOW /storage/datasets/v2 $PWD/sdf.csv /storage/plugins/*.plugin
+  python list-keys-v2 --end today --periods 3 | python list-datasets-v2 | python export-recent-datasets-v2 --since $WINDOW /storage/datasets/v2 $PWD/sdf.csv /storage/plugins/*.plugin
 
   # For now, we're only supporting complete digests. AoT_Chicago.public was the only example and was no used AFAIK.
   for p in $(ls /storage/projects | grep .complete); do
