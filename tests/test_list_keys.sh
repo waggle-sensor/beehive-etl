400082874d57b722b4cd5d21cf66370f5ebd8959
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-04-09 11:50:52 -0500

    adding list keys test

---
+++ tests/test_list_keys.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+docker build -t beehive-digest-tools:test ../beehive-digest-tools
+
+docker run -it --rm \
+    -e CASSANDRA_HOST=beehive-data.cels.anl.gov \
+    -v $PWD/projects:/storage/projects:ro \
+    -v $PWD/plugins:/storage/plugins:ro \
+    -v $PWD/datasets:/storage/datasets \
+    -v $PWD/digests:/storage/digests \
+    beehive-digest-tools:test \
+    bash list-keys-v1.sh
